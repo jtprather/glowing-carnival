3df9c49c63fcc5bca7e254d5cf5b9e12c57d97e3
jtprather <prather.justin@gmail.com>
2017-11-13 06:07:21 -0500

    Added changes to test github pages build.

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <h3 className="App-title">
+          this is another test of the commit/push build system
+        </h3>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
           this is only enough boilerplate to work.
